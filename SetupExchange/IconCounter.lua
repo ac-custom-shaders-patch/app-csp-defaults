58b61f0c2c05d33fd5ff96598689232bf66781bf
Ilja Jusupov <illvdg13@gmail.com>
2024-07-02 12:03:06 -0400

    Update IconCounter.lua

--- SetupExchange/IconCounter.lua
+++ SetupExchange/IconCounter.lua
@@ -1,18 +1,17 @@
 web.get(require('Config').endpoint..'/count?carID='..string.urlEncode(ac.getCarID(0)), function (err, response)
-  if not err then
-    local c = JSON.parse(response.body).count
-    ac.log('Available setups: %d' % c)
-    local i = ui.ExtraCanvas(64)
-    i:copyFrom('icon.png')
-    i:update(function (dt)
+  local c = not err and tonumber(JSON.parse(response.body).count)
+  if c then
+    ac.setWindowIcon('main', ui.ExtraCanvas(64, 4):update(function (dt)
+      ui.beginPremultipliedAlphaTexture()
+      ui.drawImage('icon.png', 0, 64)
       ui.beginSubtraction()
-      ui.drawRectFilled(vec2(28, 26), vec2(48, 46), rgbm.colors.white)
+      ui.drawRectFilled(vec2(28, 26), vec2(50, 46), rgbm.colors.white)
       ui.endSubtraction()
       ui.pushDWriteFont('@System;Weight=Bold')
-      ui.dwriteDrawTextClipped(math.min(c, 99), 22, vec2(15, 6), 64, ui.Alignment.Center, ui.Alignment.Center)
+      ui.dwriteDrawTextClipped(c > 99 and '99+' or c, c > 99 and 18 or 22, vec2(15, 6), 64, ui.Alignment.Center, ui.Alignment.Center)
       ui.popDWriteFont()
-    end)
-    ac.setWindowIcon('main', i)
+      ui.endPremultipliedAlphaTexture()
+    end))
   end
   exit()
 end)
