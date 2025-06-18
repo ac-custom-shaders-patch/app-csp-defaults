6a8ac3fbf7614c411e7c8f2cad165b2004cb0a47
Ilja Jusupov <illvdg13@gmail.com>
2025-06-18 14:31:51 -0400

    Update for 0.2.10

--- CarMirrorsConfigurator/CarMirrorsConfigurator.lua
+++ CarMirrorsConfigurator/CarMirrorsConfigurator.lua
@@ -2,23 +2,43 @@ local types = { [0] = 'TN', 'VA', 'IPS' }
 
 local function mirrorSettings(i)
   local p, c = ac.getRealMirrorParams(i - 1), true
+  local correctMode = ac.getPatchVersionCode() >= 3447 and p.correctMirror and not p.isMonitor
+
+  if ac.getPatchVersionCode() >= 3359 and p.notDrivenByUser then
+    ui.pushFont(ui.Font.Main)
+    ui.text('This mirror is controlled by something else and can’t be edited.')
+    ui.popFont()
+    return
+  end
 
   ui.beginGroup()
 
   ui.setNextItemWidth((ui.availableSpaceX() - 4) / 2)
-  p.rotation.x = ui.slider('##rotationX', p.rotation.x * 100, -60, 60, 'Rotation X: %.1f%%') / 100
+  p.rotation.x = ui.slider('##rotationX', p.rotation.x * 100, -60, 60, 'Horizontal rotation: %.1f%%') / 100
   ui.sameLine(0, 4)
   ui.setNextItemWidth(ui.availableSpaceX())
-  p.rotation.y = ui.slider('##rotationY', p.rotation.y * 100, -60, 60, 'Rotation Y: %.1f%%') / 100
+  p.rotation.y = ui.slider('##rotationY', p.rotation.y * 100, -60, 60, 'Vertical rotation: %.1f%%') / 100
   
-  ui.setNextItemWidth((ui.availableSpaceX() - 4) / 2)
-  p.fov = ui.slider('##fov', p.fov, 2, 60, 'FOV: %.2f°', 2)   
-  ui.sameLine(0, 4)
-  ui.setNextItemWidth(ui.availableSpaceX())
-  p.aspectMultiplier = ui.slider('##ratio', p.aspectMultiplier, 0.5, 2, 'Aspect mult.: %.2f', 1.6)   
+  if not correctMode then
+    ui.setNextItemWidth((ui.availableSpaceX() - 4) / 2)
+    p.fov = ui.slider('##fov', p.fov, 2, 60, 'FOV: %.2f°', 2)
+    ui.sameLine(0, 4)
+    ui.setNextItemWidth(ui.availableSpaceX())
+    p.aspectMultiplier = ui.slider('##ratio', p.aspectMultiplier, 0.5, 2, 'Aspect mult.: %.2f', 1.6)
+  else
+    ui.setNextItemWidth(ui.availableSpaceX())
+    p.fov = 1000 / ui.slider('##fov', 100 / (p.fov / 10), 100 / 6, 100 / 0.2, 'Curvature factor: %.0f%%', 2.527)
+    if ui.itemHovered() then
+      ui.setTooltip('For a regular flat mirror, set curvature factor to 100%')
+    end
+  end
 
-  ui.setNextItemWidth((ui.availableSpaceX() - 12) / 4)
-  if ui.checkbox('Flip X', bit.band(p.flip, ac.MirrorPieceFlip.Horizontal) ~= 0) then p.flip, c = bit.bxor(p.flip, ac.MirrorPieceFlip.Horizontal), true end
+  if not correctMode then
+    ui.setNextItemWidth((ui.availableSpaceX() - 12) / 4)
+    if ui.checkbox('Flip X', bit.band(p.flip, ac.MirrorPieceFlip.Horizontal) ~= 0) then p.flip, c = bit.bxor(p.flip, ac.MirrorPieceFlip.Horizontal), true end
+  else
+    ui.dummy(vec2((ui.availableSpaceX() - 12) / 4, 1))
+  end
   ui.sameLine(0, 4)
   ui.setNextItemWidth((ui.availableSpaceX() - 8) / 3)
   if ui.checkbox('Monitor', p.isMonitor) then p.isMonitor, c = not p.isMonitor, true end
@@ -46,6 +66,10 @@ local function mirrorSettings(i)
       ui.sameLine(0, 4)
       ui.setNextItemWidth(ui.availableSpaceX())
       p.monitorBrightness = ui.slider('##monitorBrightness', p.monitorBrightness * 100, 50, 200, 'Brightness: %.0f%%') / 100
+
+      if ui.itemHovered() then
+        ui.setTooltip('Be careful when editing brightness: different time of day, conditions and weather style might affect the overall result')
+      end
     else
       ui.setNextItemWidth(ui.availableSpaceX())
       p.monitorShaderScale.x = math.ceil(ui.slider('##monitorShaderScale.x', p.monitorShaderScale.x, 80, 800, string.format('Resolution: %.0f px', p.monitorShaderScale.x)) / 10) * 10
@@ -58,8 +82,10 @@ local function mirrorSettings(i)
   ui.endGroup()
   ui.setCursor(cur)
 
-  ui.setNextItemWidth((ui.availableSpaceX() - 12) / 4)
-  if ui.checkbox('Flip Y', bit.band(p.flip, ac.MirrorPieceFlip.Vertical) ~= 0) then p.flip, c = bit.bxor(p.flip, ac.MirrorPieceFlip.Vertical), true end
+  if not correctMode then
+    ui.setNextItemWidth((ui.availableSpaceX() - 12) / 4)
+    if ui.checkbox('Flip Y', bit.band(p.flip, ac.MirrorPieceFlip.Vertical) ~= 0) then p.flip, c = bit.bxor(p.flip, ac.MirrorPieceFlip.Vertical), true end
+  end
   
   ui.endGroup()
 
@@ -101,9 +127,44 @@ function script.windowMain(dt)
     return
   end
 
+  local mirrorNames = {}
+  local function findMirrorName(i)
+    local ret = mirrorNames[i]
+    if not ret then
+      local p = ac.getRealMirrorParams(i - 1)
+      local c = ac.getRealMirrorCount()
+      local u, t = 1, 1
+      for j = 1, c do
+        if j ~= i then
+          local o = ac.getRealMirrorParams(j - 1)
+          if o.role == p.role then
+            t = t + 1
+            if j < i then
+              u = u + 1
+            end
+          end
+        end
+      end
+      if p.role == ac.MirrorPieceRole.Left then
+        ret = 'Left mirror'
+      elseif p.role == ac.MirrorPieceRole.Top then
+        ret = 'Middle mirror'
+      elseif p.role == ac.MirrorPieceRole.Right then
+        ret = 'Right mirror'
+      else
+        ret = 'Mirror'
+      end
+      if t > 1 then
+        ret = string.format('%s (%d/%d)', ret, u, t)
+      end
+      mirrorNames[i] = ret
+    end
+    return ret
+  end
+
   ui.tabBar('mirrors', ui.TabBarFlags.IntegratedTabs, function ()
     for i = 1, ac.getRealMirrorCount() do
-      ui.tabItem(string.format('Mirror %d', i), function ()
+      ui.tabItem(findMirrorName(i), function ()
         ui.pushFont(ui.Font.Small)
         mirrorSettings(i)
         ui.popFont()
